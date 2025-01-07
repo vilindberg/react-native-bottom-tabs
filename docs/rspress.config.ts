2ca5a0b682bae9c16ff1b2f0a73ba75a86db1b9e
Oskar Kwaśniewski <oskarkwasniewski@icloud.com>
2025-01-07 11:20:13 +0100

    feat: use callstack rspress theme, remove custom styles (#229)

--- docs/rspress.config.ts
+++ docs/rspress.config.ts
@@ -1,6 +1,7 @@
 import * as path from 'path';
 import { defineConfig } from 'rspress/config';
 import { pluginOpenGraph } from 'rsbuild-plugin-open-graph';
+import { pluginCallstackTheme } from '@callstack/rspress-theme/plugin';
 
 export default defineConfig({
   root: path.join(__dirname, 'docs'),
@@ -17,17 +18,21 @@ export default defineConfig({
       {
         icon: 'github',
         mode: 'link',
-        content: 'https://github.com/okwasniewski/react-native-bottom-tabs',
+        content:
+          'https://github.com/callstackincubator/react-native-bottom-tabs',
       },
     ],
   },
-  globalStyles: path.join(__dirname, 'docs/styles/index.css'),
+  plugins: [
+    // @ts-ignore
+    pluginCallstackTheme(),
+  ],
   builderConfig: {
     plugins: [
       pluginOpenGraph({
         title: 'React Native Bottom Tabs',
         type: 'website',
-        url: 'https://okwasniewski.github.io/react-native-bottom-tabs/',
+        url: 'https://callstackincubator.github.io/react-native-bottom-tabs/',
         description: 'Native Bottom Tabs for React Native',
       }),
     ],
