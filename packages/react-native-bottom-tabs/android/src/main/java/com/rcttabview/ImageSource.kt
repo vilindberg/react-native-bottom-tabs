f2109094298d035fbd679b15e9ebd7588675a3fe
Oskar Kwaśniewski <oskarkwasniewski@icloud.com>
2025-01-17 13:29:55 +0100

    fix(android): build issues ResourceDrawableIdHelper.instance (#245)

--- packages/react-native-bottom-tabs/android/src/main/java/com/rcttabview/ImageSource.kt
+++ packages/react-native-bottom-tabs/android/src/main/java/com/rcttabview/ImageSource.kt
@@ -71,7 +71,7 @@ object ResourceIdHelper {
   }
 
   fun getResourceUri(context: Context, name: String): Uri? {
-    val drawableUri = ResourceDrawableIdHelper.instance.getResourceDrawableUri(context, name)
+    val drawableUri = ResourceDrawableIdHelper.getInstance().getResourceDrawableUri(context, name)
     if (drawableUri != Uri.EMPTY) {
       return drawableUri
     }
